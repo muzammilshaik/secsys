27f67d0fc66e3ab9e490a97741fff85d14b54db3
muzammil <muzammil.shaik55@gmail.com>
2024-12-31 17:21:41 +0530

    offers addded

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734696101',resources: [
+    cacheName: 'chirpy-1734696671',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
