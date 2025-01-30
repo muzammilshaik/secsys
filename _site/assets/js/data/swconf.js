b9126c51d71a633586f7bf75a2f2093ef3917161
muzammil <muzammil.shaik55@gmail.com>
2025-01-30 17:01:37 +0530

    add bulk cheatsheet posts

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738159844',resources: [
+    cacheName: 'chirpy-1738236430',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
