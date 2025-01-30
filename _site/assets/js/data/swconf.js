ed7f157a9b5632ff034792e69561a79bc905e8f0
muzammil <muzammil.shaik55@gmail.com>
2025-01-30 23:08:44 +0530

    added cheat sheets

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738236430',resources: [
+    cacheName: 'chirpy-1738258568',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
