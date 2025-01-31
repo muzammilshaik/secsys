fb348ae27c2bd6746962b2981af41e9d0fbbe532
muzammil <muzammil.shaik55@gmail.com>
2025-01-31 11:52:52 +0530

    updated the dietpipost name

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738304092',resources: [
+    cacheName: 'chirpy-1738304553',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
