a5482d97df435197b17666ca71c4b431b96af91b
muzammil <muzammil.shaik55@gmail.com>
2025-02-04 10:59:28 +0530

    post updated

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738645649',resources: [
+    cacheName: 'chirpy-1738646809',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
