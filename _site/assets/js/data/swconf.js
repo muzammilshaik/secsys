6185077b3e0b6ac15ca598f1028e341e501abb88
muzammilshaik <muzammil.shaik55@gmail.com>
2025-03-28 17:04:37 +0000

    updated google analytics tag

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743179651',resources: [
+    cacheName: 'chirpy-1743180718',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
